f9567318eb458a2a7a634df817de87cf0d156ba3
nqwrt <nqwrt@ymail.com>
2022-01-22 21:20:50 +0900

    commit 4

--- src/main/java/edu/kosmo/hjs/controller/HomeController.java
+++ src/main/java/edu/kosmo/hjs/controller/HomeController.java
@@ -1,7 +1,5 @@
 package edu.kosmo.hjs.controller;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
--- src/main/java/edu/kosmo/hjs/controller/RestBoardController.java
+++ src/main/java/edu/kosmo/hjs/controller/RestBoardController.java
@@ -1,37 +1,32 @@
 package edu.kosmo.hjs.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import edu.kosmo.hjs.service.BoardService;
+import edu.kosmo.hjs.vo.BoardVO;
 import lombok.extern.slf4j.Slf4j;
 
+
 @Slf4j
-@Controller
-@RequestMapping("/jboard/*")
-public class BoardController {
+@RequestMapping("/rboard/*")
+@RestController
+public class RestBoardController {
 
 	@Autowired
 	private BoardService boardService;
 
 
 	@GetMapping("/list")
-	public String view_list(Model model) {
-
-		log.info("list()...");
-		model.addAttribute("list", boardService.getList());
-		return "/board/list";
-	}
-	
-	@GetMapping("/write_view")
-	public String write_view() {
-
-		log.info("write_view()...");
+	public List<BoardVO> list(Model model) {
 		
-		return "/board/write_view";
+		log.info("list()...");
+		return boardService.getList();		
 	}
 	
 }
